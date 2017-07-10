33afb6a5fbc0e628d016d0f18a49a3317e252c5b
DIGIVENT2\WS <breakingmaddev@gmail.com>
2017-07-10 15:34:12 +0200

    Aggiornati prefab e script server graph

--- Assets/Scripts/GraphLogic.cs
+++ Assets/Scripts/GraphLogic.cs
@@ -15,9 +15,9 @@ public class GraphLogic : MonoBehaviour {
     // Use this for initialization
     void Start () {
         //Allo start deve prendere il numero di risposte dall'xml in base al contatore della domanda corrente 
-        //nAnswers = fare il parser!
+        //nAnswers = PRENDERE IL VALORE TRAMITE PARSER!
 
-        //Passa al graph il valore estratto dal file
+        //Passa al graph il valore estratto dal file. Questo metodo verrà chiamato dal server allo scadere del tempo.
         CreateGraph();
 	}
 
@@ -39,15 +39,22 @@ public class GraphLogic : MonoBehaviour {
     public void FillGraph()
     {
         float graphIncr = 1f / clientAnswers.Count;
+        //Serve a far comparire il count di quante volte è stata scelta quella domanda
+        int sameAnswers = 0;
         Debug.Log("Graph Incr: " + graphIncr);
         for (int i = 0; i < answerBar.Count; i++)
         {
+            sameAnswers = 0;
             for (int j = 0; j < clientAnswers.Count; j++)
             {
                 if(clientAnswers[j] == i)
                 {
                     Debug.Log("ClientAnswers " + j + ": " + clientAnswers[j]);
                     answerBar[i].gameObject.GetComponent<Image>().fillAmount += graphIncr;
+                    sameAnswers++;
+                    answerBar[i].gameObject.transform.GetChild(0).GetComponent<Text>().text = sameAnswers.ToString();
+                    //Qua dobbiamo inserire il testo della risposta 
+                    //answerBar[i].gameObject.transform.GetChild(1).GetComponent<Text>().text = TESTO DELLA RISPOSTA;
                     Debug.Log("nClient: " + clientAnswers.Count);
                 }
             }         
