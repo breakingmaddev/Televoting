3dbd4071b0decb6d9b70ca3c536d70c9bbf14b40
DIGIVENT2\WS <breakingmaddev@gmail.com>
2017-07-11 11:59:35 +0200

    - Modificato Client Class per poter vedere i valori da Inspector - Modifiche minori al Canvas Server - Modifiche minori all'Answer Button - Aggiornato il PlayerCanvas

--- Assets/Scripts/ClientsClass.cs
+++ Assets/Scripts/ClientsClass.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class ClientsClass : MonoBehaviour, IComparable<ClientsClass>
+[Serializable]
+public class ClientsClass
 {
 
     public string nameClient;
@@ -13,15 +14,5 @@ public class ClientsClass : MonoBehaviour, IComparable<ClientsClass>
     {
         nameClient = _name;
         answerChoose = _answerIndex;
-    }
-
-    public int CompareTo(ClientsClass other)
-    {
-        if (other == null)
-        {
-            return 1;
-        }
-
-        return answerChoose - other.answerChoose;
     }
 }
